539cbb84f455939a5ef72b4ca5daf43da265c073
Noxolo <wendyngcobo98@gmail.com>
2024-04-03 21:32:50 +0200

    Create h1 element for heading

--- index.js
+++ index.js
@@ -25,22 +25,23 @@ document.addEventListener('DOMContentLoaded', function () {
       }
   
       // 🚨 Generate certificate content dynamically
-      certificateContent. = `
-      <h3>${studentName}</h3>
-    `;
+      const certHeading = document.createElement('h1');
+      certHeading.textContent = 'Certificate of Achievement';
+      certificateContent.textContent = `
+      <h3>${studentName}</h3>`;
     
       //  Display the modal
       modal.style.display = 'block';
   
       // Clear the form inputs
-      studentNameInput.value = '';
-      personalMessageInput.value = '';
-      if(courseNameInput) courseNameInput.value = '';
+      // studentNameInput.value = '';
+      // personalMessageInput.value = '';
+      // if(courseNameInput) courseNameInput.value = '';
     });
   
     //  🚨 Close the modal when the close button is clicked
-    closeModal.('', function () {
-      
-    });
+    // closeModal.addEventListener('click', function () {
+    //   modal.style.display = 'none';
+    // });
   });
   
